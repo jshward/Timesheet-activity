4592d46ab3c0770e5a82d8a809a7239900501b14
jshward <jshward069@gmail.com>
2018-03-06 19:25:43 -0700

    initial js push

---
+++ assets/js/logic.js
@@ -0,0 +1,36 @@
+
+$(document).ready(function () {
+	// Initialize Firebase
+	var config = {
+		apiKey: "AIzaSyAm86W1l6VdNdoltxfM7t2dPo5aI3fg7ac",
+		authDomain: "timesheet-b52be.firebaseapp.com",
+		databaseURL: "https://timesheet-b52be.firebaseio.com",
+		projectId: "timesheet-b52be",
+		storageBucket: "timesheet-b52be.appspot.com",
+		messagingSenderId: "1005863832565"
+	};
+	firebase.initializeApp(config);
+
+	var database = firebase.databse()
+
+
+	$("#add-employee-btn").on("click", function (event) {
+		event.preventDefault();
+
+		// Grabbed values from text boxes
+		name = $("#employee-name-input").val().trim();
+		role = $("#role-input").val().trim();
+		startDate = $("#start-input").val().trim();
+		payRate = $("#rate-input").val().trim();
+
+		// Code for handling the push
+		database.ref().push({
+			name: name,
+			role: role,
+			startDate: startDate,
+			monthly: payRate,
+		});
+
+	});
+
+});
